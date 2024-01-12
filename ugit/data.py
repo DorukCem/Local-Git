198a82690c3fd2bb24af01cdd0fc55ae2d4b30f6
Doruk Cem <doruk.cem@gmail.com>
2024-01-12 13:11:27 +0300

    checkout + fixed write tree

--- ugit/data.py
+++ ugit/data.py
@@ -4,18 +4,17 @@ import os
 GIT_DIR = '.ugit'
 
 def init ():
-   os.makedirs (GIT_DIR)
-   os.makedirs (f'{GIT_DIR}/objects')
-
+   os.makedirs(os.path.join(GIT_DIR, 'objects'))
+   
 def hash_object (data, type_='blob'):
    obj = type_.encode () + b'\x00' + data
-   oid = hashlib.sha1 (obj).hexdigest ()
-   with open (f'{GIT_DIR}/objects/{oid}', 'wb') as out:
-      out.write (obj)
+   oid = hashlib.sha1(obj).hexdigest ()
+   with open(os.path.join(GIT_DIR, 'objects', oid), 'wb') as out:
+      out.write(obj)
    return oid
 
 def get_object (oid, expected='blob'):
-   with open (f'{GIT_DIR}/objects/{oid}', 'rb') as f:
+   with open(os.path.join(GIT_DIR, 'objects', oid), 'rb') as f:
       obj = f.read ()
 
    type_, _, content = obj.partition (b'\x00')
@@ -26,11 +25,11 @@ def get_object (oid, expected='blob'):
    return content
 
 def set_HEAD(oid):
-   with open(f"{GIT_DIR}/HEAD", "w") as f:
+   with open(os.path.join(GIT_DIR, 'HEAD'), "w") as f:
       f.write(oid)
 
 def get_HEAD():
-   if os.path.isfile(f"{GIT_DIR}/HEAD"):
-      with open (f"{GIT_DIR}/HEAD") as f:
+   head_file = os.path.join(GIT_DIR, 'HEAD')
+   if os.path.isfile(head_file):
+      with open(head_file) as f:
          return f.read().strip()
-      
